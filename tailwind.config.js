e9e7fe3ef5f919e3c6bde78f04af33385935dfc3
Iris van Ollefen <irisvanollefen@gmail.com>
2021-05-23 11:34:06 +0200

    Added safelist for widths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ["./index.html", './src/**/*.{js,ts,jsx,tsx}'],
+  purge: {
+    content: ["./index.html", './src/**/*.{js,ts,jsx,tsx}'],
+    options: {
+      safelist: ['w-1/12', 'w-2/12', 'w-3/12', 'w-4/12', 'w-5/12', 'w-6/12', 'w-7/12', 'w-8/12', 'w-9/12', 'w-10/12', 'w-11/12', 'w-12/12']
+    }
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
